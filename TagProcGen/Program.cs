bc0ec2c7c7629c72fcec8a1b53c0a6506f549d93
brent113 <bscrivner@gmail.com>
2019-12-23 16:45:51 -0800

    Add command line args and change to console app

--- TagProcGen/Program.cs
+++ TagProcGen/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,11 +13,35 @@ namespace TagProcGen
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        static int Main(string[] args)
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FormMain());
+            if (args?.Length > 0)
+            {
+                if (args.Length != 1 || !File.Exists(args[0]))
+                {
+                    WriteUsage();
+                    return 1;
+                }
+
+                GenTags.Generate(args[0]);
+            }
+            else
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new FormMain());
+            }
+
+            return 0;
+        }
+
+        static void WriteUsage()
+        {
+            Console.WriteLine("TagProcGen");
+            Console.WriteLine("RTAC and OSI SCADA Configuration Builder");
+            Console.WriteLine("");
+            Console.WriteLine("Usage:");
+            Console.WriteLine("  TagProcGen.exe path_to_configuration.xls[x]");
         }
     }
 }
